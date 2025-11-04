70d030367385c22e4587a37796a2cc0f4d18af83
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-04 14:48:34 +0600

    HomeWorkTask01 executed

---
+++ CSE111 Fall2025 Lab Assignments 3/ToyTester.java
@@ -0,0 +1,46 @@
+class Toy {
+
+    public String name;
+    public int price;
+
+    public Toy(String name, int price) {
+        this.name = name;
+        this.price = price;
+
+        System.out.println("A new toy has been made!");
+    }
+
+    public void updatePrice(int price) {
+        this.price = price;
+    }
+
+    public void showPrice() {
+        System.out.println("price: " + this.price + " Taka");
+    }
+
+    public void updateName(String name) {
+        
+        System.out.println("Changing old name: " + this.name);
+        
+        this.name = name;
+        System.out.println("new name: " + this.name);
+    }
+}
+
+public class ToyTester{
+    public static void main(String[] args){
+        Toy t1 = new Toy("Car", 230);
+        System.out.println("1=================");
+        t1.updatePrice(250);
+        System.out.println("2=================");
+        System.out.println(t1.name);
+        t1.showPrice();
+        System.out.println("3=================");
+        Toy t2 = new Toy("Robot", 450);
+        System.out.println("4=================");
+        t2.updateName("Autobot");
+        System.out.println("5=================");
+        System.out.println(t2.name);
+        t2.showPrice();
+    }
+}
