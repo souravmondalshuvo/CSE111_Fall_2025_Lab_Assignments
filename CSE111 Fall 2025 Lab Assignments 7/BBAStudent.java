90c4596ea63c8b63f9186e4a4aa2a2dd8606073c
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-12-18 00:51:51 +0600

    CSE111 Fall 2025 Lab Assignment 7 HomeWorkTask02 Completed

---
+++ CSE111 Fall 2025 Lab Assignments 7/BBAStudent.java
@@ -0,0 +1,14 @@
+//HomeWorkTask02
+
+public class BBAStudent extends Student{
+    
+    public BBAStudent() {
+        setName("Default");
+        setDepartment("BBA");
+    }
+
+    public BBAStudent(String name) {
+        setName(name);
+        setDepartment("BBA");
+    }
+}
